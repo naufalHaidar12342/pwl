f11865d78ef2b6bcb7867f2de02411d937bd850f
naufalHaidar12342 <1111912342@mhs.dinus.ac.id>
2021-03-07 10:52:31 +0700

    mencoba print/cetak dengan posisi berbeda

---
+++ latihan/variabel.php
@@ -0,0 +1,10 @@
+<?php
+    $nim = "A11.2019.12342";
+    $nama = 'Naufal';
+    
+    echo "NIM : " . $nim . "<br>";
+    echo "Nama : $nama<br>";
+    echo"$nim <br>";
+    $_nama2='john';
+    echo "$_nama2 is his name";
+?>
