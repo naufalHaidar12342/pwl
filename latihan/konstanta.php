323a7162276ea4b6c81e5a70109492f216fc058e
naufalHaidar12342 <1111912342@mhs.dinus.ac.id>
2021-03-07 22:33:46 +0700

    menambahkan konstant.php

---
+++ latihan/konstanta.php
@@ -0,0 +1,13 @@
+<?
+    //define seperti assign variabel
+    // di python (nama variabel = nilai/value/isinya)
+    define ("NAMA", "Sutrisno ");
+    define ("NILAI", 90);
+    echo NAMA;
+    // echo "Nama : <br>" ,NAMA ;
+    // echo "<br>Nilai :  <br>" ,NILAI;
+
+    /*  define("NAMA","tony");
+     echo("Nama =" .NAMA); */
+
+?>
--- latihan/variabel.php
+++ latihan/variabel.php
@@ -1,8 +1,13 @@
 <?php
     $nim = "A11.2019.12342";
     $nama = 'Naufal';
-    
+
+    //di baris bawah ini persis, 
+    //sama seperti di pyhton yang
+    //print("nim : " +nim+ "bla bla") 
     echo "NIM : " . $nim . "<br>";
+    
+    //layaknya tag <br> di html, memberi break//
     echo "Nama : $nama<br>";
     echo"$nim <br>";
     $_nama2='john';
