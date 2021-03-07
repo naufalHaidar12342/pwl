a7b052248a0a8a519281bc86e80f76104936e795
naufalHaidar12342 <1111912342@mhs.dinus.ac.id>
2021-03-08 00:07:02 +0700

    menambahkan ss  settingan vscode untuk php

--- latihan/konstanta.php
+++ latihan/konstanta.php
@@ -1,11 +1,11 @@
 <?php
-    //define seperti assign variabel
-    // di python (nama variabel = nilai/value/isinya)
+    /* //define seperti assign variabel
+    // di python (nama variabel = nilai/value/isinya) */
     define ("NAMA", "Sutrisno ");
     define ("NILAI", 90);
     echo NAMA;
-    // echo "Nama : <br>" ,NAMA ;
-    // echo "<br>Nilai :  <br>" ,NILAI;
+    /* // echo "Nama : <br>" ,NAMA ;
+    // echo "<br>Nilai :  <br>" ,NILAI; */
 
     /*  define("NAMA","tony");
      echo("Nama =" .NAMA); */
