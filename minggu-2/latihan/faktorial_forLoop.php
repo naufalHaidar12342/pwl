cb445835f807bb491e19c49f4908a6277ec4d9c9
naufalHaidar12342 <1111912342@mhs.dinus.ac.id>
2021-03-17 14:47:53 +0700

    menambahkan sumber referensi program sekaligus push

--- minggu-2/latihan/faktorial_forLoop.php
+++ minggu-2/latihan/faktorial_forLoop.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>faktorial dengan for loop</title>
+    <!-- sumber referensi : https://www.javatpoint.com/php-factorial-program -->
 </head>
 
 <body>
