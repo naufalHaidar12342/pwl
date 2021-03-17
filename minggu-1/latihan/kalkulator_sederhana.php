9600eddcf972452a25b1c736f342723fc16ae204
naufalHaidar12342 <1111912342@mhs.dinus.ac.id>
2021-03-17 18:13:28 +0700

    berhasil memperbaiki kalkulator, namun tampilan hasil belum rapi

--- minggu-1/latihan/kalkulator_sederhana.php
+++ minggu-1/latihan/kalkulator_sederhana.php
@@ -19,6 +19,73 @@
     </style>
 </head>
 <body>
+
+<?php
+    /* bagian di bawah terinspirasi dari
+    github fabiyudzaky*/ 
+    
+        $bil_1=0;
+        $bil_2=0;
+        $hasil=0;
+        $operasiMath=0;  
+        
+        // inisiasi membaca isi form
+    if(isset($_POST['operasiMath'])){
+        $operasiMath=$_POST['operasiMath'];
+    }
+
+    if(isset($_POST['bil_1'])){
+        $bil_1=$_POST['bil_1'];
+    }
+
+    if(isset($_POST['bil_2'])){
+        $bil_2=$_POST['bil_2'];
+    }
+        
+    if (isset($_POST['hasil'])) {
+        $hasil=$_POST['hasil'];
+    }
+    else {
+        $i=1;
+    }
+    
+    if ($operasiMath=='+') {
+        $hasil= $bil_1 + $bil_2;
+        echo "hasilnya adalah ".$hasil;
+        // return $hasil;
+    }
+    else if ($operasiMath=='-') {
+        $hasil= $bil_1 - $bil_2;
+        echo "hasilnya adalah ".$hasil;
+        // return $hasil;
+    }
+    
+    else if ($operasiMath=='*') {
+        $hasil= $bil_1 * $bil_2;
+        echo "hasilnya adalah ".$hasil;
+        // return $hasil;
+    }
+    
+    else if ($operasiMath=='/') {
+        $hasil= $bil_1 / $bil_2;
+        echo "hasilnya adalah ".$hasil;
+        // return $hasil;
+    }
+
+
+    else {
+        $hasil=$hasil;
+        // echo "hasilnya adalah ".$hasil;
+    }
+    # code...
+    
+    
+    /* bagian di bawah membaca input dari user */
+    
+        
+    ?>
+
+    
     <form action="" method="post">
         <!-- bilangan pertama -->
         <label for="">bil. pertama :</label>
@@ -42,69 +109,13 @@
         </select>
 
         <br>
-        <label for="hasil">Hasil : </label>
-        <input type="text-area" name="hasil" disabled value="<?php echo $hasil;?>">
+        <!-- <label for="hasil">Hasil : </label> -->
+        <input type="submit" name ="hasil" value="hasil"> <?php echo $hasil;?>
     
     </form>
     
 
-    <?
-    /* bagian di bawah terinspirasi dari
-    github fabiyudzaky*/ 
-    if ($_POST) {
-        $bil_1=0;
-        $bil_2=0;
-        $hasil=0;
-        $operasiMath=0;  
-        
-        // inisiasi membaca isi form
-        if(isset($_POST['operasiMath'])){
-            $operasiMath=$_POST['operasiMath'];
-            
-
-        if(isset($_POST['bil_1'])){
-            $bil_1=$_POST['bil_1'];
-        }
-
-        if(isset($_POST['bil_2'])){
-            $bil_2=$_POST['bil_2'];
-        }
-        
-        if (isset($_POST['hasil'])) {
-            $hasil=$_POST['hasil'];
-        }
-        if ($operasiMath=='+') {
-            $hasil= $bil_1 + $bil_2;
-            return $hasil;
-        }
     
-        else if ($operasiMath=='-') {
-            $hasil= $bil_1 - $bil_2;
-            return $hasil;
-        }
-    
-        else if ($operasiMath=='*') {
-            $hasil= $bil_1 * $bil_2;
-            return $hasil;
-        }
-    
-        else if ($operasiMath=='/') {
-            $hasil= $bil_1 / $bil_2;
-            return $hasil;
-        }
-
-        else {
-            $hasil=$hasil;
-            return $hasil;
-        }
-    }# code...
-    }
-    
-    
-    /* bagian di bawah membaca input dari user */
-    
-        
-    ?>
 </body>
 </html>
 
