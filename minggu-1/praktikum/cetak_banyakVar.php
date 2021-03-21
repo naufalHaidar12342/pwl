2e3617cded5648dd8d743fb4314c3a38f1c54fcb
naufalHaidar12342 <1111912342@mhs.dinus.ac.id>
2021-03-21 15:25:32 +0700

    mencoba beragam operasi matematika

---
+++ minggu-1/praktikum/cetak_banyakVar.php
@@ -0,0 +1,15 @@
+<?php
+    $a=8+4;
+    $b=9-2;
+    $c=2*3;
+    $d=10+3-7*4;
+    $e=((10+3)-7)*4;
+    $f=-79;
+
+    echo "isi variabel a : $a <br>";
+    echo "isi variabel b : $b <br>";
+    echo "isi variabel c : $c <br>";
+    echo "isi variabel d : $d <br>";
+    echo "isi variabel e : $e <br>";
+    echo "isi variabel f : $f <br>";
+?>
