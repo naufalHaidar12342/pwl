d0037e9642db3603a07780c666d822fc30804b15
naufalHaidar12342 <1111912342@mhs.dinus.ac.id>
2021-04-15 13:52:58 +0700

    melengkapi folder praktikum minggu 3

---
+++ minggu-3/praktikum/input_combobox.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="proses_combobox.php" method="POST" name="input">
+        <h2>Pilih Film Kartun Favorit Anda :</h2><select name="kartun">
+            <option value="Sponge Bob">Sponge Bob</option>
+            <option value="Sinchan">Sinchan</option>
+            <option value="Conan">Conan</option>
+            <option value="Doraemon">Doraemon</option>
+            <option value="Dragon Ball">Dragon Ball</option>
+            <option value="Naruto">Naruto</option>
+        </select><input type="submit" name="Pilih" value="Pilih">
+    </form>
+</body>
+
+</html>
---
+++ minggu-3/praktikum/input_tipe_textarea.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="proses_textarea.php" method="post" name="input">
+        <h2>Input Kritik / Saran :</h2><textarea name="saran" cols="40" rows="5"></textarea><br><input type="submit" name="Proses" value="">
+    </form>
+
+
+
+
+</body>
+
+</html>
